2e1cc2b1f995c9e2402434003f77561f7f49f4c5
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-08-30 12:51:41 +0800

    C++: Leetcode-8 String to Integer

---
+++ Leetcode-C++/Leetcode-8 String to Interger.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <climits>
+#include <cctype>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+        while (i < n && s[i] == ' ') i++;
+
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
+            i++;
+        }
+        
+        long long result = 0;
+        while (i < n && isdigit(s[i])) {
+            result = result * 10 + (s[i] - '0');
+            if (sign == 1 && result > INT_MAX) return INT_MAX;
+            if (sign == -1 && -result < INT_MIN) return INT_MIN;
+            i++;
+        }
+        
+        return static_cast<int>(sign * result);
+    }
+};
