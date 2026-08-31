ee68c05185fa51af5efae5a3b938fd5835b5e29b
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-08-31 12:52:48 +0800

    C++: Leetcode-9 Parlindromic Numbers

---
+++ Leetcode-C++/Leetcode-9 Parlindromic Numbers.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       string xx = to_string(x);
+       string temp = "";
+       for (int i = 0; i < xx.size(); i++) {
+            temp = xx[i] + temp;
+       } 
+       if (temp == xx) {
+        return true;
+       } else {
+        return false;
+       }
+    }
+};
+
+bool isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+    
+    int reverted = 0;
+    while (x > reverted) {
+        reverted = reverted * 10 + x % 10;
+        x /= 10;
+    }
+    
+    return x == reverted || x == reverted / 10;
+}
+// 纯数学解法 额外空间O(1)
+
+bool isPalindrome(int x) {
+    string xx = to_string(x);
+    string temp = xx;
+    reverse(temp.begin(), temp.end());
+    return temp == xx;
+}
+// C++的reverse写法 
