5c8fe12f16607c0027de85b0cf2aed95754ffec0
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-09-01 20:09:48 +0800

    C++: Leetcode-11 Container With Most Water

---
+++ Leetcode-C++/Leetcode-11 Container with most water.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int result = 0, left = 0, right = height.size()-1, length = height.size()-1;
+        while (left < right) {
+            length = right - left;
+            result = max(result, min(height[left], height[right]) * length);
+            if (height[left] < height[right]) {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        return result;
+    }
+};
