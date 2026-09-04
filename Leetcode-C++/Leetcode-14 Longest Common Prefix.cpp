ecdc85d0c191c0a9b01f98b1ec9d41b3d62d35c2
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-09-04 23:55:48 +0800

    C++: Leetcode-14 Longest Common Prefix

---
+++ Leetcode-C++/Leetcode-14 Longest Common Prefix.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool judge(vector<string>& strs, int i) {
+        char temp = strs[0][i];
+        for (int j = 1; j < strs.size(); j++) {
+            if (i >= strs[j].size() || temp != strs[j][i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = "";
+        for (int i = 0; i < strs[0].size(); i++) {
+            if (judge(strs, i)) {
+                res = res + strs[0][i];
+            } else {
+                return res;
+            }
+        }
+        return res;
+    }
+};
