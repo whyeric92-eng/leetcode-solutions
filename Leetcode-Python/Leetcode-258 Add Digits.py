c083d797cd8fb530e83ded90da76eee55d458075
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-09-09 16:29:28 +0800

    Python: Leetcode-258 Add Digits

---
+++ Leetcode-Python/Leetcode-258 Add Digits.py
@@ -0,0 +1,16 @@
+﻿class Solution:
+    def addDigits(self, num: int) -> int:
+        while(num >= 10):
+            res = 0
+            for part in str(num):
+                res += int(part)
+            num = res
+        return num
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
+# O(1) 数学技巧
+# 一个数 mod 9 的结果，恒等于它各位数字之和 mod 9
